ecacda7cf1b0c2a0018c1f39bc2cda1604b626ab
Bruno Diaz <d1rtyskittl3z@gmail.com>
2025-11-17 22:02:19 -0600

    Merging Eighth Edition files from CC

--- src/components/frames/groups/Misc-2.ts
+++ src/components/frames/groups/Misc-2.ts
@@ -7,7 +7,7 @@ const group: LegacyGroup = {
     { kind: 'label', label: 'Old' },
     { kind: 'pack', label: 'Future Shifted', id:'FutureRegular' },
     { kind: 'pack', label: 'Colorshifted', id:'8thColorshifted' },
-    { kind: 'pack', label: '8th Edition', id:'8th' },
+    { kind: 'pack', label: 'Eighth Edition', id:'8th' },
     { kind: 'pack', label: 'Seventh Edition', id:'Seventh' },
     { kind: 'pack', label: 'Fifth Edition', id:'SeventhButFifth' },
     { kind: 'pack', label: 'Fourth Edition', id:'Fourth' },
@@ -15,7 +15,8 @@ const group: LegacyGroup = {
     { kind: 'pack', label: 'Alpha/Beta/Unlimited', id:'ABU' },
 
     { kind: 'label', label: 'Misc' },
-    { kind: 'pack', label: '8th Edition Playtest Cards', id:'8thPlaytest' },
+    { kind: 'pack', label: 'Eighth Edition Universes Beyond', id:'8thUB' },
+    { kind: 'pack', label: 'Eighth Edition Playtest Cards', id:'8thPlaytest' },
     { kind: 'pack', label: 'Playtest Cards', id:'Playtest' },
     { kind: 'pack', label: 'Dungeon (AFR)', id:'Dungeon' },
     { kind: 'pack', label: 'Planechase', id:'Planechase' },
